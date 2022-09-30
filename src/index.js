4f45d8988a98e2f90a521164a64b8652bef74685
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-09-29 23:45:19 -0300

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -24,6 +24,16 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  return statement.reduce((accumulator, operation) => {
+    if (operation.type === 'credit') {
+      return accumulator + operation.amount;
+    } else {
+      return accumulator - operation.amount;
+    }
+  }, 0);
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -65,6 +75,29 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
     amount,
     created_at: new Date(),
     type: 'credit',
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({
+      error: 'Insufficient funds.',
+    });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
   };
 
   customer.statement.push(statementOperation);
